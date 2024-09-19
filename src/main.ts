200f53ce3c246c78c9a5e292ae0af2aa9ca359fb
Edw1nM4rquez <edwi1999@hotmail.com>
2024-09-18 23:27:18 -0500

    Delete space in the main

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,6 @@ import { AppModule } from './app.module';
 //import { ValidateInputPipe } from './core/pipes/validate.pipe';
 import * as cookieParser from 'cookie-parser';
 import { UniqueConstraintFilterFilter } from './filters/unique-constraint-filter.filter';
-
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
